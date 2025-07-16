12a11e672f3bd22317ec654588bc45fc0ed0263c
preciousono <onoprecious246@gmail.com>
2025-07-16 20:19:00 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Precious Ono | Portfolio';
+export const SITE_DESCRIPTION = 'Precious Ono: Your Digital Marketing Partner. I help businesses get noticed online, attract customers, and drive real growth through expert SEO, social media, and content strategies.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
